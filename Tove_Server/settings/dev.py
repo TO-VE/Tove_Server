18b8c6c4cd39b042f896a721f17bad0ca0bd9404
chaeri93 <chaeri93@ewhain.net>
2022-07-02 02:44:45 +0900

    init

---
+++ Tove_Server/settings/dev.py
@@ -0,0 +1,16 @@
+from .base import *  # noqa
+import pymysql
+pymysql.install_as_MySQLdb()
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
+    }
+}
