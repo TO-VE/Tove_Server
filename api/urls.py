2d6613bac6f29b2cde394aa255eb4368ca4bc836
chaeri93 <chaeri93@ewhain.net>
2022-07-02 07:29:35 +0900

    feat: grouppurchase api view

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from api.views import HelloView
+from api.views import *
 
 urlpatterns = [
-    path('hello', HelloView.as_view()),
+    path('challenge', ChallengeView.as_view()),
+    path('challenge/<int:pk>', ChallengeDetail.as_view()),
+    path('purchase', ChallengeView.as_view()),
+    path('purchase/<int:pk>', ChallengeDetail.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -5,42 +5,19 @@ from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from api.models import Challenge
-from api.serializers import ChallengeSerializer
+from api.models import Challenge, GroupPurchase
+from api.serializers import ChallengeSerializer, GroupPurchaseSerializer
 
 
-class UserIsAnonymous(Exception):
-    pass
-
-
-def user_authenticate(request):
-    if request.user.is_anonymous:
-        raise UserIsAnonymous
-
-class HelloView(APIView):
-    def get(self, request):
-        return Response("GET Hello", status=200)
-
-    def post(self, request):
-        return Response("POST Hello", status=200)
-
-    def patch(self, request):
-        return Response("PATCH Hello", status=200)
-
-    def delete(self, request):
-        return Response("DELETE Hello", status=200)
-
-
+# 함께해요 챌린지
 class ChallengeView(APIView):
     def get(self, request):
-        user_authenticate(request)
         challenges = Challenge.objects.all().order_by('-created_at')
         serializer = ChallengeSerializer(challenges, many=True)
         return JsonResponse(serializer.data, status=200, safe=False)
 
 
     def post(self, request):
-        user_authenticate(request)
         serializer = ChallengeSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -59,7 +36,41 @@ class ChallengeDetail(APIView):
 
     def patch(self, request, pk):
         challenge = self.get_object(pk)
-        serializer = ChallengeSerializer(challenge, data=request.data, partial=True) # set partial=True to update a data partially
+        serializer = ChallengeSerializer(challenge, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+# 같이 사요 공동구매
+class PurchaseView(APIView):
+    def get(self, request):
+        purchases = GroupPurchase.objects.all().order_by('-created_at')
+        serializer = GroupPurchaseSerializer(purchases, many=True)
+        return JsonResponse(serializer.data, status=200, safe=False)
+
+
+    def post(self, request):
+        serializer = GroupPurchaseSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+class PurchaseDetail(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(GroupPurchase, pk=pk)
+
+    def get(self, request, pk):
+        purchase = self.get_object(pk)
+        serializer = GroupPurchaseSerializer(purchase)
+        return JsonResponse(serializer.data, status=201)
+
+    def patch(self, request, pk):
+        purchase = self.get_object(pk)
+        serializer = GroupPurchaseSerializer(purchase, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data, status=201)
