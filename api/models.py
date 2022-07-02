d55489b095f3f092d21d26b4f5e98aac22f92a38
chaeri93 <chaeri93@ewhain.net>
2022-07-02 10:34:18 +0900

    chore: alter User model

--- api/models.py
+++ api/models.py
@@ -1,9 +1,13 @@
-from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
 
 class UserManager(BaseUserManager):
     def create_user(self, nickname, email, password, **kwargs):
+        if not email:
+            raise ValueError('Users must have an email address')
+
         user = self.model(
             nickname=nickname,
             email=email,
@@ -12,11 +16,24 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
+    def create_superuser(self, nickname=None, email=None, password=None, **extra_fields):
+        superuser = self.create_user(
+            nickname=nickname,
+            email=email,
+            password=password,
+        )
+        superuser.is_superuser = True
+        superuser.is_active = True
+        superuser.save(using=self._db)
+        return superuser
 
-class User(models.Model):
+
+class User(AbstractBaseUser, PermissionsMixin):
     nickname = models.CharField(max_length=40, unique=True)
     email = models.EmailField(max_length=30, unique=True, null=False, blank=False)
-
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
     objects = UserManager()
 
     USERNAME_FIELD = 'nickname'
